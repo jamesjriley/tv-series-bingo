3266669336b5e61c476a5a453370326138b7eb62
James Riley <expatty@gmail.com>
2023-06-26 19:47:04 +1200

    Added imdb title and synopsis

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from imdb import IMDb
+from flask import Flask, render_template, request
+
+import json
 
 app = Flask(__name__, static_folder='client/build', static_url_path='/')
 
@@ -9,6 +12,32 @@ def index():
 @app.route('/api')
 def get_data():
     return {'message': 'Hello, world!'}
+
+@app.route('/api/events', methods=['GET'])
+def get_events():
+    show_name = request.args.get('showName', default = '', type = str)
+    with open('events.json', 'r') as f:
+        events = json.load(f)
+    return events
+
+@app.route('/api/showinfo', methods=['GET'])
+def get_show_info():
+    show_name = request.args.get('showName', default = '', type = str)
+    ia = IMDb()
+    movies = ia.search_movie(show_name)
+    if movies:
+        movie = movies[0]  # Get the first movie that matches the search
+        ia.update(movie)  # Update movie with more detailed info
+        plot = movie.data.get('plot', ["No plot available."])
+        title = movie.data.get('title', "No title available.")
+        year = movie.data.get('year', "No year available.")
+        url = ia.get_imdbURL(movie)
+        result = {"showName": show_name, "plot": plot, "title": title, "year": year, "url": url}
+    else:
+        result = {"showName": show_name, "plot": "No show found.", "title": "", "year": "", "url": ""}
+    return result
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  fetch('/').then(response => response.text()).then(message => console.log(message));
+  const [showName, setShowName] = useState('');
+  const [synopsis, setSynopsis] = useState('');
+  const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [url, setUrl] = useState('');
+  const [events, setEvents] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch(`/api/showinfo?showName=${showName}`);
+    const data = await response.json();
+    setSynopsis(data.plot);
+    setTitle(data.title);
+    setYear(data.year);
+    setUrl(data.url);
+    const eventsResponse = await fetch(`/api/events?showName=${showName}`);
+    const eventsData = await eventsResponse.json();
+    setEvents(eventsData);
+};
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React today
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          TV Show Name:
+          <input type="text" value={showName} onChange={e => setShowName(e.target.value)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {title && year && url && <h1><a href={url}>{title} ({year})</a></h1>}
+      {synopsis && <p>Synopsis: {synopsis}</p>}
+      {events && events.map((event, index) => (
+        <p key={index}>{event.event}: {event.likelihood}% likelihood</p>
+      ))}
     </div>
   );
 }
